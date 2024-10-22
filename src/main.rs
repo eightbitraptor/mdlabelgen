63eb9141cfdf257adb83fb35ffc8e112e7d6d23f
Matt Valentine-House <matt@eightbitraptor.com>
2024-10-22 14:53:50 +0100

    Add GPL-2.0 license header

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
+/*
+ * MDLabelgen: A program to generate label images for (primarily audio) MiniDiscs.
+ *
+ * Copyright (C) 2024 Matt Valentine-House
+ *
+ * SPDX-License-Identifier: GPL-2.0-only
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, version 2.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See
+ * the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If
+ * not, see <https://www.gnu.org/licenses/>.
+ */
+
 use std::error::Error;
 use std::fs;
 
